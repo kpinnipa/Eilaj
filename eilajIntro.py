aa0338f6bee32f23fdc6b5f386b3d6eaa3abcbfb
mli43 <kate.li.0924@gmail.com>
2019-04-07 01:09:41 -0700

    eilaj Intro

--- eilajIntro.py
+++ eilajIntro.py
@@ -2,15 +2,19 @@ def intro(questions, answers):
     print('Hello! I am Lily, your special representative from red cross. This is a fun game to get a sense of how you are feeling,')
     print('How was your trip:\nA. Good\nB. Bad\nC. Okay')
     x = input('Type letter: ')
-    if x.find('A') != -1 or x.find('a') != -1:
-        answers.append('Good')
-    elif x.find('B') != -1 or x.find('b') != -1:
-        answers.append('Bad')
-    elif x.find('C') != -1 or x.find('c') != -1:
-        answers.append('Okay')
-    elif x.upper() not in "ABC":
-        print("Please choose between A, B, and C.")
-        x = input('Type letter: ')
+    while True:
+        if x.find('A') != -1 or x.find('a') != -1:
+            answers.append('Good')
+            break
+        elif x.find('B') != -1 or x.find('b') != -1:
+            answers.append('Bad')
+            break
+        elif x.find('C') != -1 or x.find('c') != -1:
+            answers.append('Okay')
+            break
+        else:
+            print("Please choose between A, B, and C.")
+            x = input('Type letter: ')
     questions.append('How was your trip?')
     print('Are you alone?\nA. Yes\nB. No')
     x = input('Type letter: ')
