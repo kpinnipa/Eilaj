63b5a077e0835d5234c1aca77eec9a904ed5e032
mli43 <kate.li.0924@gmail.com>
2019-04-06 21:31:20 -0700

    playground conversation

---
+++ eilaj.py
@@ -0,0 +1,43 @@
+# Playground conversation
+
+
+def main():
+    questions = ("Do you have other friends here?\n", "How often do you play here?\n")
+    answers = [("A. Yes", "B. No"), ("A. All the time", "B. Never", "C. Sometimes")]
+    result = []
+    counter = 0
+    while counter < len(questions):
+        print(questions[counter])
+        print_list(answers[counter])
+        answer = input()
+        try:
+            index = translate(answer)
+            result.append(answers[counter][index][3:])
+        except IndexError:
+            print("I'm sorry but can you say that again? I didn't quite get what you were saying.")
+        #print(result)
+        counter += 1
+    print(result)
+
+
+
+
+def translate(answer):
+    #try:
+        list = ["A", "B", "C", "D", "E", "F", "G"]
+        index = list.index(answer)
+        return index
+    #except IndexError:
+        #continue
+        #pass
+
+
+
+def print_list(list):
+    for i in list:
+        print(i)
+
+
+
+if __name__ == "__main__":
+    main()
