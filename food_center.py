56676d2495f0f27276740e30ce57010813d51533
mli43 <kate.li.0924@gmail.com>
2019-04-06 23:04:59 -0700

    food center complete

---
+++ food_center.py
@@ -0,0 +1,38 @@
+# Playground conversation
+
+
+def main():
+    questions = ("Are you often hungry?\n", "Do you have a good appetite?\n")
+    answers = [("A. Yes", "B. No", "C. Maybe"), ("A. Yes", "B. No", "C. Mainly")]
+    result = []
+    counter = 0
+    while counter < len(questions):
+        print(questions[counter])
+        print_list(answers[counter])
+        answer = input()
+        try:
+            index = translate(answer)
+            result.append(answers[counter][index][3:])
+        except IndexError:
+            print("I'm sorry but can you say that again? I didn't quite get what you were saying.")
+        counter += 1
+    print(result)
+
+
+
+
+def translate(answer):
+        list = ["A", "B", "C", "D", "E", "F", "G"]
+        index = list.index(answer)
+        return index
+
+
+
+def print_list(list):
+    for i in list:
+        print(i)
+
+
+
+if __name__ == "__main__":
+    main()
